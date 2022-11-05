a0e2e7c08fcf7c8341fa281fb6c037739de28eb3
jeppebayer <jeppebayer@gmail.com>
2022-11-05 17:22:35 +0100

    05/11-2022

--- scripts/02_data_preparation/02_00_init_data_prep.sh
+++ scripts/02_data_preparation/02_00_init_data_prep.sh
@@ -22,17 +22,17 @@
 
 # ----------------- Configuration ----------------------------------------
 
-# Directory containing scripts (Do NOT end with '/')
-scripts="people/Jeppe_Bayer/scripts/02_data_preparation"
+# Directory containing scripts, abosolute path (Do NOT end with '/')
+scripts="/home/jepe/EcoGenetics/people/Jeppe_Bayer/scripts/02_data_preparation"
 
-# Species specific reference genome (in FASTA format)
-RG="BACKUP/reference_genomes/Orchesella_cincta/GCA_001718145.1/GCA_001718145.1_ASM171814v1_genomic.fna"
+# Species specific reference genome, abosolute path (in FASTA format)
+RG="/home/jepe/EcoGenetics/BACKUP/reference_genomes/Orchesella_cincta/GCA_001718145.1/GCA_001718145.1_ASM171814v1_genomic.fna"
 
-# Species specific sample directory (Do NOT end with '/')
-SD="BACKUP/population_genetics/collembola/Orchesella_cincta"
+# Species specific sample directory, abosolute path (Do NOT end with '/')
+SD="/home/jepe/EcoGenetics/BACKUP/population_genetics/collembola/Orchesella_cincta"
 
-# Working directory (Do NOT end with '/')
-WD="people/Jeppe_Bayer/steps"
+# Working directory, abosolute path (Do NOT end with '/')
+WD="/home/jepe/EcoGenetics/people/Jeppe_Bayer/steps"
 
 # ----------------- Script Queue -----------------------------------------
 
@@ -64,34 +64,34 @@ for sample in "$SD"/*; do
                 [[ -d $WD/01_data_preparation/$(basename $SD)/"$(basename "$sample")"/post_filter_stats ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"/"$(basename "$sample")"/post_filter_stats
                 
                 # AdapterRemoval
-                jid1=$(sbatch --parsable $scripts/02_01_data_prep.sh $RG $SD $WD "$sample")
+                jid1=$(sbatch --parsable "$scripts"/02_01_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
                 # Aligning to reference
-                jid2_1=$(sbatch --parsable --dependency=aftok:"$jid1" $scripts/02_02_01_data_prep.sh $RG $SD $WD "$sample")
-                jid2_2=$(sbatch --parsable --dependency=aftok:"$jid1" $scripts/02_02_02_data_prep.sh $RG $SD $WD "$sample")
+                jid2_1=$(sbatch --parsable --dependency=afterany:"$jid1" "$scripts"/02_02_01_data_prep.sh "$RG" "$SD" "$WD" "$sample")
+                jid2_2=$(sbatch --parsable --dependency=afterany:"$jid1" "$scripts"/02_02_02_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
                 # Merging of alignment files
-                jid3=$(sbatch --parsable --dependency=afterok:"$jid2_1":"$jid2_2" $scripts/02_03_data_prep.sh $RG $SD $WD "$sample")
+                jid3=$(sbatch --parsable --dependency=afterany:"$jid2_1":"$jid2_2" "$scripts"/02_03_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
                 # Marking duplicates
-                jid4=$(sbatch --parsable --dependency=afterok:"$jid3" $scripts/02_04_data_prep.sh $RG $SD $WD "$sample")
+                jid4=$(sbatch --parsable --dependency=afterany:"$jid3" "$scripts"/02_04_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
                 # Statistics pre-filtering
-                jid5_1=$(sbatch --parsable --dependency=afterok:"$jid4" $scripts/02_05_01_data_prep.sh $RG $SD $WD "$sample")
-                jid5_2=$(sbatch --parsable --dependency=afterok:"$jid4" $scripts/02_05_02_data_prep.sh $RG $SD $WD "$sample")
-                jid5_3=$(sbatch --parsable --dependency=afterok:"$jid4" $scripts/02_05_03_data_prep.sh $RG $SD $WD "$sample")
-                jid5_4=$(sbatch --parsable --dependency=afterok:"$jid4" $scripts/02_05_04_data_prep.sh $RG $SD $WD "$sample")
+                jid5_1=$(sbatch --parsable --dependency=afterany:"$jid4" "$scripts"/02_05_01_data_prep.sh "$RG" "$SD" "$WD" "$sample")
+                jid5_2=$(sbatch --parsable --dependency=afterany:"$jid4" "$scripts"/02_05_02_data_prep.sh "$RG" "$SD" "$WD" "$sample")
+                jid5_3=$(sbatch --parsable --dependency=afterany:"$jid4" "$scripts"/02_05_03_data_prep.sh "$RG" "$SD" "$WD" "$sample")
+                jid5_4=$(sbatch --parsable --dependency=afterany:"$jid4" "$scripts"/02_05_04_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
 
                 # Removal of duplicates, unmapped reads and low quality mappings
-                jid6=$(sbatch --parsable --dependency=afterok:"$jid5_1":"$jid5_2":"$jid5_3":"$jid5_4" $scripts/02_06_data_prep.sh $RG $SD $WD "$sample")
+                jid6=$(sbatch --parsable --dependency=afterany:"$jid5_1":"$jid5_2":"$jid5_3":"$jid5_4" "$scripts"/02_06_data_prep.sh "$RG" "$SD" "$WD" "$sample")
 
                 # Statistics post-filtering
-                sbatch --dependency=afterok:"$jid6" $scripts/02_07_01_data_prep.sh $RG $SD $WD "$sample"
-                sbatch --dependency=afterok:"$jid6" $scripts/02_07_02_data_prep.sh $RG $SD $WD "$sample"
-                sbatch --dependency=afterok:"$jid6" $scripts/02_07_03_data_prep.sh $RG $SD $WD "$sample"
-                sbatch --dependency=afterok:"$jid6" $scripts/02_07_04_data_prep.sh $RG $SD $WD "$sample"
-                sbatch --dependency=afterok:"$jid6" $scripts/02_07_05_data_prep.sh $RG $SD $WD "$sample"
+                sbatch --dependency=afterany:"$jid6" "$scripts"/02_07_01_data_prep.sh "$RG" "$SD" "$WD" "$sample"
+                sbatch --dependency=afterany:"$jid6" "$scripts"/02_07_02_data_prep.sh "$RG" "$SD" "$WD" "$sample"
+                sbatch --dependency=afterany:"$jid6" "$scripts"/02_07_03_data_prep.sh "$RG" "$SD" "$WD" "$sample"
+                sbatch --dependency=afterany:"$jid6" "$scripts"/02_07_04_data_prep.sh "$RG" "$SD" "$WD" "$sample"
+                sbatch --dependency=afterany:"$jid6" "$scripts"/02_07_05_data_prep.sh "$RG" "$SD" "$WD" "$sample"
 
             else
 
--- scripts/test1.sh
+++ scripts/test1.sh
@@ -3,52 +3,90 @@
 #SBATCH --partition normal
 #SBATCH --mem-per-cpu 2G
 #SBATCH --cpus-per-task 1
-#SBATCH --time 00:10:00
+#SBATCH --time 00:30:00
 
+# ------------------------------------------------------------------------
+# Created by Jeppe Bayer
+# 
+# Master's student at Aarhus University
+# Department of Genetics, Ecology and Evolution
+# Centre for EcoGenetics
+# 
+# For troubleshooting or questions:
+# Email: jeppe.bayer@bio.au.dk
+# ------------------------------------------------------------------------
+
+# ----------------- Description ------------------------------------------
+
+# Script for initializing data preparation procedure for sequence data >70MB
+
+# ----------------- Configuration ----------------------------------------
+
+# Directory containing scripts (Do NOT end with '/')
+scripts="people/Jeppe_Bayer/scripts"
+
+# Species specific reference genome (in FASTA format)
+RG="BACKUP/reference_genomes/Orchesella_cincta/GCA_001718145.1/GCA_001718145.1_ASM171814v1_genomic.fna"
+
+# Species specific sample directory (Do NOT end with '/')
 SD="BACKUP/population_genetics/collembola/Orchesella_cincta"
 
+# Working directory (Do NOT end with '/')
 WD="people/Jeppe_Bayer/steps"
 
-# for sample in "$SD"/*; do
-    
-#     if [ "$(ls -A "$sample")" ]; then
-#         for file in "$sample"/*.bam; do
-#             if [ ! -e "$file" ]; then
-#                 echo "$sample has not been processed"
-#                 [[ -d $WD/01_data_preparation/$(basename $SD)/"$(basename "$sample")" ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"/"$(basename "$sample")"
+# ----------------- Script Queue -----------------------------------------
 
-#             else
-#                 echo "$sample has been processed, $file"
-#             fi
-#         done
-#     else
-#         echo "$sample is empty"
-#     fi
-# done
+# # Creates temp directory in working directory if none exist
+# [[ -d $WD/temp ]] || mkdir -m 764 $WD/temp
 
+# # Creates 01_data_preparation directory in working directory if none exist
+# [[ -d $WD/01_data_preparation ]] || mkdir -m 764 $WD/01_data_preparation
 
-
-
-
+# # Creates species directory in 01_data_preparation directory if none exist
 # [[ -d $WD/01_data_preparation/$(basename $SD) ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"
 
-# sbatch people/Jeppe_Bayer/scripts/test2.sh $WD
+# # Loops through all sample folders within species specific sample directory
+# for sample in "$SD"/*; do
 
+#     # Checks if sample folder is empty
+#     if [ "$(ls -A "$sample")" ]; then
 
+#         for file in "$sample"/*.bam; do
 
+#             # Checks whether a .bam file already exists within sample folder, indicating samples have already been processed
+#             if [ ! -e "$file" ]; then
+                
+#                 # Creates sample directory in species directory if none exist
+#                 [[ -d $WD/01_data_preparation/$(basename $SD)/"$(basename "$sample")" ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"/"$(basename "$sample")"
+                
+#                 # Creates pre- and post-filtering directory in sample directory if none exist
+#                 [[ -d $WD/01_data_preparation/$(basename $SD)/"$(basename "$sample")"/pre_filter_stats ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"/"$(basename "$sample")"/pre_filter_stats
+#                 [[ -d $WD/01_data_preparation/$(basename $SD)/"$(basename "$sample")"/post_filter_stats ]] || mkdir -m 764 "$WD"/01_data_preparation/"$(basename $SD)"/"$(basename "$sample")"/post_filter_stats
+                
+#                 # AdapterRemoval
+#                 jid1=$(sbatch --parsable /home/jepe/EcoGenetics/people/Jeppe_Bayer/scripts/test2.sh)
 
+#                 # Aligning to reference
+#                 sbatch --parsable --dependency=aftany:"$jid1" /home/jepe/EcoGenetics/people/Jeppe_Bayer/scripts/test3.sh
+                
+#             else
 
-R1=
+#                 srun echo "$sample already contains a .bam file, $file, and is skipped"
+#             fi
 
-for R2 in BACKUP/population_genetics/collembola/Orchesella_cincta/Ocin_NYS-F/* ; do
-    if [ "$R1" ] ; then
-        # 2nd entry - pair
-        srun echo "R1 is $R1, R2 is $R2"
-    else
-        # First entry - just remember.
-        R1=$R2
-    fi
-done
+#         done
 
+#     else
+
+#         srun echo "$sample is an empty directory and is skipped"
+#     fi
+
+# done
+
+# AdapterRemoval
+jid1=$(sbatch --parsable /home/jepe/EcoGenetics/people/Jeppe_Bayer/scripts/test2.sh)
+
+# Aligning to reference
+sbatch --parsable --dependency=aftany:"$jid1" /home/jepe/EcoGenetics/people/Jeppe_Bayer/scripts/test3.sh
 
 exit 0
